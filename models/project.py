b16fdb5ebebc80a8bbf62ff3d529eef97fac44f3
Cecilia <cecilia.cunha2004@gmail.com>
2025-06-27 01:31:17 -0300

    feat: Adiciona modelos de entidade Project e Task

---
+++ models/project.py
@@ -0,0 +1,74 @@
+class Project:
+    def __init__(self, id, name, description, start_date, due_date, status, user_id):
+        # Atributos do projeto(privados para encapsulamento)
+        self._id = id
+        self._name = name
+        self._description = description
+        self._start_date = start_date
+        self._due_date = due_date
+        self._status = status
+        self._user_id = user_id 
+
+        # Getters (Métodos para obter os valores dos atributos)
+        def get_id(self):
+            return self._id
+        
+        def get_name(self):
+            return self._name
+        
+        def get_description(self):
+            return self._description
+        
+        def get_start_date(self):
+            return self._start_date
+        
+        def get_due_date(self):
+            return self._due_date
+        
+        def get_status(self):
+            return self._status
+        
+        def get_user_id(self):
+            return self._user_id
+        
+        # Setters (Métodos para definir/modificar os valores dos atributos)
+        def set_name(self, name):
+            self._name = name
+
+        def set_description(self, description):
+            self._description = description
+        
+        def set_start_date(self, start_date):
+            self._start_date = start_date
+        
+        def set_due_date(self, due_date):
+            self._due_date = due_date
+
+        def set_status(self, status):
+            self._status = status
+
+        # Método para converter o objeto em um dicionário
+        def to_dict(self):
+            return {
+                "id": self._id,
+                "name": self._name,
+                "description": self._description,
+                "start_date": self._start_date,
+                "due_date": self._due_date,
+                "status": self._status,
+                "user_id": self._user_id
+            }
+        
+        # Método de classe para criar um objeto Project a partir de um dicionário
+        @classmethod
+        def from_dict(cls, data):
+            return cls(
+                data["id"],
+                data["name"],
+                data["description"],
+                data["start_date"],
+                data["due_date"],
+                data["status"],
+                data["user_id"]
+            )
+
